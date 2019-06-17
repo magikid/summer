a9ad9f473f4c7098df2604ef2becd4c561001c97
Chris W Jones <chris.jones@cbsinteractive.com>
2019-06-17 19:56:07 -0400

    Initial commit

---
+++ src/main/java/dev/foobarbaz/App.java
@@ -0,0 +1,15 @@
+package dev.foobarbaz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        dataSet1 = new CsvDataSet("ufo_sighting_data.csv");
+        dataSet2 = new JsonDataSet("")
+        System.out.println( "Hello World!" );
+    }
+}
