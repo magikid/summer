208389e965b90895cabcd37ae4b6626f7a785b57
Chris W Jones <chris.jones@cbsinteractive.com>
2019-07-02 09:38:43 -0400

    Adds writing results to file

--- src/main/java/dev/foobarbaz/UfoWitchData.java
+++ src/main/java/dev/foobarbaz/UfoWitchData.java
@@ -2,16 +2,22 @@ package dev.foobarbaz;
 
 import dev.foobarbaz.Ufo.UfoDatum;
 import dev.foobarbaz.Witch.WitchDatum;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 
-import java.security.cert.CollectionCertStoreParameters;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.ZoneId;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class UfoWitchData {
+class UfoWitchData {
     private List<UfoDatum> ufoDatumList;
     private List<WitchDatum> witchDatumList;
     public enum UserSelection {UFO,WITCH};
+    private static final String OUTPUT_LOCATION = "output.csv";
 
     public UfoWitchData(List<UfoDatum> ufoDatumList, List<WitchDatum> witchDatumList, UserSelection selection) {
         this.ufoDatumList = ufoDatumList;
@@ -45,6 +51,8 @@ public class UfoWitchData {
         System.out.println("========================================================");
         System.out.println("Number found: " + filteredUfo.toArray().length);
         filteredUfo.forEach(System.out::println);
+        System.out.println("Writing data");
+        writeUfoData(filteredUfo);
     }
 
     private void printWitchData() {
@@ -61,5 +69,37 @@ public class UfoWitchData {
         System.out.println("========================================================");
         System.out.println("Number found: " + witchMonths.toArray().length);
         witchMonths.forEach(System.out::println);
+        System.out.println("Writing data");
+        writeWitchData(witchMonths);
+    }
+
+    private void writeWitchData(Collection<WitchDatum> data) {
+        try {
+            var writer = Files.newBufferedWriter(Paths.get(OUTPUT_LOCATION));
+            CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT.withHeader("city", "month", "name"));
+            for (var datum : data) {
+                csvPrinter.printRecord(datum.getCity(), datum.getMonthAccused(), datum.getName());
+            }
+            csvPrinter.flush();
+        } catch (IOException e) {
+            System.err.println("Error writing output");
+            System.err.println(e.getMessage());
+            e.printStackTrace(System.err);
+        }
+    }
+
+    private void writeUfoData(Collection<UfoDatum> data) {
+        try {
+            var writer = Files.newBufferedWriter(Paths.get(OUTPUT_LOCATION));
+            CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT.withHeader("city", "desc", "date"));
+            for (var datum : data) {
+                csvPrinter.printRecord(datum.getCity(), datum.getDescription(), datum.getDate());
+            }
+            csvPrinter.flush();
+        } catch (IOException e) {
+            System.err.println("Error writing output");
+            System.err.println(e.getMessage());
+            e.printStackTrace(System.err);
+        }
     }
 }
